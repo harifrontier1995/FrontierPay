4effc705e9ccd112a86d56e0bf6ba8e20900b65e
harifrontier1995 <harishankar@frontier.xyz>
2022-05-02 13:03:04 +0530

    10 mins for otp

--- app/models/verification.rb
+++ app/models/verification.rb
@@ -1,6 +1,6 @@
 class Verification < ApplicationRecord
 
 	def is_valid? otp
-	 	otp_code == otp && (sent_time + 30.minutes) > Time.now
+	 	otp_code == otp && (sent_time + 10.minutes) > Time.now
 	end
 end
